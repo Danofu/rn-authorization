3eb82cb262f4f30cb574659a7828420382d04f30
Daniil Borysov <danofu13@gmail.com>
2023-04-15 16:31:47 +0200

    Fetch protected message

--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { AuthContext } from 'auth-context';
+
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+  const { token } = useContext(AuthContext);
+
+  useEffect(() => {
+    (async () => {
+      const { data: message } = await axios.get(
+        `https://react-native-course-6f73f-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${token}`
+      );
+      setFetchedMessage(message);
+    })();
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
--- util/auth.js
+++ util/auth.js
@@ -4,13 +4,8 @@ const API_KEY = 'AIzaSyDYMeioiXq2_YrvGiOZsxdYIu-J1GV0u9Y';
 
 export const authenticate = async (mode, email, password) => {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
-  const response = await axios.post(url, {
-    email,
-    password,
-    returnSecureToken: true,
-  });
-
-  return response.data.idToken;
+  const { data } = await axios.post(url, { email, password, returnSecureToken: true });
+  return data.idToken;
 };
 
 export const createUser = (email, password) => authenticate('signUp', email, password);
