2b60e4e3743f4a8feba19343803cc5646318c738
Daniil Borysov <danofu13@gmail.com>
2023-04-15 14:17:23 +0200

    Signup user in SignupScreen

--- screens/SignupScreen.jsx
+++ screens/SignupScreen.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AuthContent from 'Auth/AuthContent';
+import LoadingOverlay from 'ui/LoadingOverlay';
+import { createUser } from 'auth';
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const signupHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user ..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
